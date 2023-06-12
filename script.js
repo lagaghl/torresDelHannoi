f7320ffe55be046417c151cc66f55c4c1a436697
lagaghl <jhonatan.nieva323@gmail.com>
2023-06-11 20:47:20 -0500

    Just change a little spaces

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ const closeButton = document.getElementById('modal-close');
 const msgFinal = document.getElementById('msg');
 let fichaArrastrada;
 let movimientos = 0; 
-
+function finDelJuego() {
+    msgFinal.innerHTML = `Has realizado ${movimientos} movimientos`;
+    modal.style.display = 'grid';
+    let torre1= document.getElementById('tower1');
+    fichas.forEach(ficha=>{
+        torre1.appendChild(ficha);
+        ficha.removeAttribute('draggable');
+    });
+}
 fichas.forEach(ficha=>{
     ficha.addEventListener('mouseover',(e)=>{
         if(e.target.parentElement.lastElementChild===ficha) {
@@ -33,19 +41,7 @@ torres.forEach(torre =>{
         }
     })
 });
-
-function finDelJuego() {
-    msgFinal.innerHTML = `Has realizado ${movimientos} movimientos`;
-    modal.style.display = 'grid';
-    let torre1= document.getElementById('tower1');
-    fichas.forEach(ficha=>{
-        torre1.appendChild(ficha);
-        ficha.removeAttribute('draggable');
-    });
-}
-
 closeButton.addEventListener('click', ()=>{
     modal.style.display = 'none';
     movimientos=0;
-
 });
